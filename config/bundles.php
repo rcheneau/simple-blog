941fbc5e2f0a944fead77c3137f23717892a58a8
rcheneau <raphael.cheneau@sipartech.com>
2022-09-06 11:25:03 +0200

    Add paginator bundle to paginate blog posts

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,6 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
--- src/Controller/BlogPostController.php
+++ src/Controller/BlogPostController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
-use LogicException;
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class BlogPostController extends AbstractController
 {
-    #[Route(path: '/', name: 'app_blog_post_list')]
-    public function list(): Response
+    #[Route(path: '/{page}', name: 'app_blog_post_list')]
+    public function list(EntityManagerInterface $em, PaginatorInterface $paginator, int $page = 1): Response
     {
-        return $this->render('blog_post/list.html.twig');
+        $pagination = $paginator->paginate(
+            $em->createQuery('SELECT p FROM App\Entity\BlogPost p'),
+            $page,
+            10
+        );
+
+        return $this->render(
+            'blog_post/list.html.twig',
+            [
+                'pagination' => $pagination,
+            ]
+        );
     }
 }
