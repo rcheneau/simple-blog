c8267ea9953669f67a67b0b17a97ad80cc3c6fac
rcheneau <raphael.cheneau@sipartech.com>
2022-09-07 17:10:26 +0200

    Set layout navbar

---
+++ src/Controller/LayoutController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
+
+class LayoutController extends AbstractController
+{
+    /** @noinspection PhpUnused */
+    public function navbar(RequestStack $requestStack, Security $security): Response
+    {
+        $navbarItems = [
+            ['text' => 'route.name.home', 'name' => 'app_blog_post_list', 'icon' => 'fa-solid fa-house'],
+        ];
+
+        $userDropdownItems = $security->isGranted('ROLE_USER')
+            ? [
+                ['text' => 'route.name.my_account', 'name' => 'app_account', 'icon' => 'fa-solid fa-user'],
+                ['text' => 'route.name.logout', 'name' => 'app_logout', 'icon' => 'fa-solid fa-right-from-bracket'],
+            ]
+            : [
+                ['text' => 'route.name.login', 'name' => 'app_login', 'icon' => 'fa-solid fa-user'],
+            ];
+
+        return $this->render('layout/_navbar.html.twig', [
+            'navbarItems'       => $navbarItems,
+            'userDropdownItems' => $userDropdownItems,
+            'currentRoute'      => $requestStack->getMainRequest()->attributes->get('_route'),
+        ]);
+    }
+}
