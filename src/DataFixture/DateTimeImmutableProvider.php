e0633efcdaaeee7499526668a41637773c5f97a2
rcheneau <raphael.cheneau@sipartech.com>
2022-09-08 14:14:36 +0200

    Refactor code & fix phpstan errors

--- src/DataFixture/DateTimeImmutableProvider.php
+++ src/DataFixture/DateTimeImmutableProvider.php
@@ -5,21 +5,17 @@ declare(strict_types=1);
 namespace App\DataFixture;
 
 use DateTimeImmutable;
-use DateTimeInterface;
 use Faker\Provider\DateTime;
 
-/**
- * Extends faker's DateTime provider to be able to do the same as dateTimeBetween but with immutable DateTime.
- * Code taken from: https://github.com/nelmio/alice/issues/1096#issuecomment-1156175048
- */
 final class DateTimeImmutableProvider extends DateTime
 {
     /** @noinspection PhpUnused */
-    public static function dateTimeImmutableBetween($startDate = '-30 years', $endDate = 'now', $timezone = null): DateTimeImmutable
+    public static function dateTimeImmutableBetween(
+        \DateTime|string $startDate = '-30 years',
+        \DateTime|string $endDate = 'now',
+        string|null      $timezone = null
+    ): DateTimeImmutable
     {
-        $startTimestamp = $startDate instanceof DateTimeInterface ? $startDate->getTimestamp() : $startDate;
-        $dateTime = parent::dateTimeBetween($startTimestamp, $endDate, $timezone);
-
-        return DateTimeImmutable::createFromMutable($dateTime);
+        return DateTimeImmutable::createFromMutable(parent::dateTimeBetween($startDate, $endDate, $timezone));
     }
 }
