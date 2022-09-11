eb4ea81363d45f9c681b94c2a1e352aa145902a9
Raphaël Cheneau <raphael@cheneau.me>
2022-09-11 23:20:26 +0200

    Config test db & script to setup

---
+++ scripts/setup_test.sh
@@ -0,0 +1,7 @@
+echo "# Create database & add admin user & load fixtures"
+php bin/console doctrine:database:drop --force --if-exists --env=test
+php bin/console doctrine:database:create --env=test
+php bin/console doctrine:migrations:migrate --no-interaction --env=test
+php bin/console hautelook:fixtures:load --no-interaction --env=test
+php bin/console app:create-user -a admin@email.test admin password --env=test
+echo -e " --> DONE\n"
