d01ba1ce6f4ad35d1399d53d87ec1613372f4a98
rcheneau <raphael.cheneau@sipartech.com>
2022-09-12 14:29:27 +0200

    Update test security controller login

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -8,22 +8,22 @@ class SecurityControllerTest extends WebTestCase
 {
     public function testLogin()
     {
-        $client = static::createClient();
+        $client  = static::createClient();
         $crawler = $client->request('GET', '/en/login');
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('title', 'Login');
 
         $form = $crawler->filter('[type=submit]')->form();
 
-        $form['email'] = 'admin@email.test';
+        $form['email']    = 'admin@email.test';
         $form['password'] = 'password';
 
         $client->submit($form);
 
         $this->assertResponseStatusCodeSame(302);
 
+        $client->followRedirects();
         $client->request('GET', '/en/logout');
-        $this->assertResponseStatusCodeSame(302);
-        $crawler = $client->followRedirect();
+        $this->assertSelectorTextContains('a', 'Login');
     }
 }
