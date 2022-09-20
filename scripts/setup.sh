ec32487604b827d39f5dff968f5d281f2e6bb500
Raphaël Cheneau <raphael@cheneau.me>
2022-09-20 22:41:54 +0200

    Update setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,6 @@ echo "# Create database & add admin user & load fixtures"
 php bin/console doctrine:database:drop --force --if-exists
 php bin/console doctrine:database:create
 php bin/console doctrine:migrations:migrate --no-interaction
+rm public/media/cache/thumbnail/*
 php bin/console hautelook:fixtures:load --no-interaction
-php bin/console app:create-user -a admin@email.test admin password
 echo -e " --> DONE\n"
