c34a993d6cee29000b6247ecf43e060757fa2d40
Raphaël Cheneau <raphael@cheneau.me>
2022-09-20 20:25:51 +0200

    Add test admin image

--- tests/Controller/AdminControllerTest.php
+++ tests/Controller/AdminControllerTest.php
@@ -2,8 +2,11 @@
 
 namespace App\Tests\Controller;
 
+use App\DataFixture\ImageProcessor;
 use App\Repository\BlogPostRepository;
+use App\Repository\ImageRepository;
 use DateTimeImmutable;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class AdminControllerTest extends AbstractControllerTest
 {
@@ -81,5 +84,49 @@ class AdminControllerTest extends AbstractControllerTest
         $this->assertEquals('Updated content', $updatedPost->getContent());
         $this->assertEquals($loggedUser->getId(), $updatedPost->getUpdatedBy()->getId());
         $this->assertEquals($updatedPost->getUpdatedAt()->format('Y-m-d'), (new DateTimeImmutable())->format('Y-m-d'));
+    }
+
+    public function testAdminImageManage()
+    {
+        $client = static::createClient();
+        $this->login($client);
+        $client->request('GET', '/fr/admin/images');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('title', 'Gestion des images');
+    }
+
+    public function testCreateImage()
+    {
+        $client = static::createClient();
+
+        /** @var ImageRepository $imageRepository */
+        $imageRepository = static::getContainer()->get(ImageRepository::class);
+        $count = $imageRepository->count([]);
+
+        $this->login($client);
+
+        $path = __DIR__ . '/../../src/DataFixture/assets';
+        $image =  current(array_diff(scandir(__DIR__ . '/../../src/DataFixture/assets'), ['.', '..']));
+        $uploadedFile = new UploadedFile(
+            "$path/$image",
+            $image
+        );
+
+        $crawler = $client->request('GET', '/fr/admin/images/create');
+        $this->assertResponseIsSuccessful();
+
+        $form = $crawler->filter('form')->form();
+        $form['image[title]'] = 'Image title';
+        $form['image[description]'] = 'My description...';
+        $form['image[file][file]'] = $uploadedFile;
+        $client->submit($form);
+
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+
+        $this->assertSelectorTextContains('title', 'Gestion des images');
+
+        $this->assertEquals($count + 1, $imageRepository->count([]));
     }
 }
