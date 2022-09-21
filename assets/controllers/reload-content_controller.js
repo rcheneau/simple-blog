ad915161e063e439e8a77ac9a9522f0f184a9235
Raphaël Cheneau <raphael@cheneau.me>
2022-09-21 20:42:54 +0200

    Fix bug reload content nesting same tag

--- assets/controllers/reload-content_controller.js
+++ assets/controllers/reload-content_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
         const res = await fetch(e.detail.url, {
             headers: {'X-Requested-with': 'XMLHttpRequest'},
         });
-        this.element.innerHTML = await res.text();
+        this.element.outerHTML = await res.text();
         history.pushState({ }, '', e.detail.url);
         this.element.scrollIntoView();
     }
