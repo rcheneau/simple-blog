834454ab8ee89fafcfe8e4bd4c580e5d0d4fc2c4
Raphaël Cheneau <raphael@cheneau.me>
2022-09-21 13:54:32 +0200

    Remove import

--- src/Form/ImageType.php
+++ src/Form/ImageType.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Form;
 
-use App\Entity\Image;
 use App\Models\Input\ImageInput;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
