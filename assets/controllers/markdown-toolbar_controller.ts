aa5fa787cb2c46b8ef1d11dc849501a6bb278768
Raphaël Cheneau <raphael@cheneau.me>
2022-09-23 15:51:06 +0200

    Add markdown toolbar

---
+++ assets/controllers/markdown-toolbar_controller.ts
@@ -0,0 +1,10 @@
+import { Controller } from '@hotwired/stimulus';
+import '@github/markdown-toolbar-element';
+
+// noinspection JSUnusedGlobalSymbols
+/* stimulusFetch: 'lazy' */
+export default class extends Controller<HTMLElement> {
+    connect() {
+
+    }
+}
