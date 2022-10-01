28c4c6f38c8ae917d8aea544dfb657c8372fb758
Raphaël Cheneau <raphael@cheneau.me>
2022-10-01 18:51:44 +0900

    Update setup script to properly clean images & their thumbnails

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@ echo "# Create database & add admin user & load fixtures"
 php bin/console doctrine:database:drop --force --if-exists
 php bin/console doctrine:database:create
 php bin/console doctrine:migrations:migrate --no-interaction
-rm public/media/cache/thumbnail/*
+rm -f public/media/cache/image_500_500/*
+rm -f var/storage/images/*
 php bin/console hautelook:fixtures:load --no-interaction
 echo -e " --> DONE\n"
