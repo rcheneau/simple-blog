caebdefb85b44f950758868ab2a7b02b3cfaad37
Raphaël Cheneau <raphael@cheneau.me>
2022-11-17 13:21:12 +0900

    Fix bug previous and next button navigator didn't work on reloadable contents

--- assets/controllers/reload-content_controller.ts
+++ assets/controllers/reload-content_controller.ts
@@ -2,16 +2,31 @@ import { Controller } from '@hotwired/stimulus';
 
 // noinspection JSUnusedGlobalSymbols
 export default class extends Controller<HTMLElement> {
+    scrollToTopOfElement = false;
+
+    connect() {
+        const self = this;
+        window.onpopstate = async function () {
+            await self.update(window.location.href, self.scrollToTopOfElement);
+        };
+    }
+
     async reload(event: CustomEvent) {
-        const res = await fetch(event.detail.url, {
+        await this.update(event.detail.url, event.detail.scrollToTopOfElement);
+        history.pushState({ }, '', event.detail.url);
+    }
+
+    async update(url: string, scrollToTopOfElement = false) {
+        this.scrollToTopOfElement = scrollToTopOfElement;
+
+        const res = await fetch(url, {
             headers: {'X-Requested-with': 'XMLHttpRequest'},
         });
 
-        if (event.detail.scrollToTopOfElement) {
+        if (scrollToTopOfElement) {
             this.element.scrollIntoView();
         }
 
         this.element.outerHTML = await res.text();
-        history.pushState({ }, '', event.detail.url);
     }
 }
